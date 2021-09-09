6be4f2724337cddf221682f5c74d88c58bd0f965
Jordan Black <jblack530@gmail.com>
2021-09-09 16:50:27 -0700

    added tailwind react native classnames, uber logo img and finished basic homescreen styling, started navoptions component

--- App.js
+++ App.js
@@ -3,19 +3,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import HomeScreen from './screens/HomeScreen';
+
 import { store } from './store';
-
-
-// 1) Set up Redux
-
 
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <Text>LETS BUILD UBER!</Text>
-      </View>
+    <Provider store={store} >
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ components/NavOptions.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const data = [
+    {
+        id: '123',
+        title: 'Get a ride',
+        image: "https://links.papareact.com/3pn",
+        screen: "MapScreen",
+    },
+    {
+        id: '456',
+        title: 'Order food',
+        image: 'https://links.papareact.com/28w',
+        screen: "EatsScreen"
+    }
+]
+
+const NavOptions = () => {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    )
+}
+
+export default NavOptions
+
+
