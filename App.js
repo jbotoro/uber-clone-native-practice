3e7c9cc950554e552b256ee356afd6daf5e4425b
Jordan Black <jblack530@gmail.com>
2021-09-09 17:43:38 -0700

    added flatlist element and buttons for ride or food, basic style with tw on Touchable Opacity element and View element in NavOptions and Icons from react-native-elements

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
-
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import HomeScreen from './screens/HomeScreen';
-
 import { store } from './store';
 
 
 export default function App() {
   return (
     <Provider store={store} >
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
