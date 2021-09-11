0ea0d7630f74f11c1fc5762c1e65f7257fcdccf7
Jordan Black <jblack530@gmail.com>
2021-09-10 17:16:35 -0700

    added react navigation, brief setup add navigation to navoptions button for mapscreen

--- App.js
+++ App.js
@@ -3,15 +3,35 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import  MapScreen  from './screens/MapScreen';
 import { store } from './store';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store} >
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+          <Stack.Navigator>
+            <Stack.Screen 
+              name="HomeScreen" 
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name="MapScreen" 
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
